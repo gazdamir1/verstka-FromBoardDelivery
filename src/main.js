0464e30539e0909c77e767da0ae4c2ee129b13d2
gazdamir1 <gazdamir00@gmail.com>
2026-05-06 15:45:23 +0300

    fixed: deploy

--- src/main.js
+++ src/main.js
@@ -1,2 +1,4 @@
 import "./components/header/header.js"
 import "./components/reviewsBlock/reviewsBlock.js"
+import "./components/calculatorBlock/calculatorBlock.js"
+import "./components/contactBlock/contactBlock.js"
